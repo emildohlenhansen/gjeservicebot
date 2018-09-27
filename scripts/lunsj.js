cd0cf6779c211a749a4a2325e7d68d81dcb4fa76
emildohlenhansen <emil.dohlen.hansen@bekk.no>
2018-09-27 13:44:42 +0200

    fix bugs

--- scripts/lunsj.js
+++ scripts/lunsj.js
@@ -1,12 +1,15 @@
-//Commands
+// Commands
 // lunsj - vis hele ukens lunsjmeny
 // lunsj dag - lunsj meny for gitt dag
 // lunsj dag sted - lunsj for gitt dag på gitt sted
 // lunsj sted - lunsj for sted hele uken
 
 const menyer = require('../static/lunsj');
+
 const CronJob = require('cron').CronJob;
+
 const lunsjvarsler = {};
+
 const ukedager = [
   'mandag',
   'tirsdag',
@@ -16,31 +19,33 @@ const ukedager = [
 ];
 
 const logRespond = (robot, res) => {
-  robot.logger.info(`Respond - user: ${res.message.user.name}, command ${res.match[0]}`);
+  robot.logger.info(`Respond - user: ${res.message.user.name}, command: ${res.match[0]}`);
 };
 
-const capitalize = string = (
+const capitalize = string => (
   string.charAt(0).toUpperCase() + string.slice(1)
 );
 
 const ukemeny = (data, sted) => {
   const meny = Object.keys(data).map(key => `\n\t${capitalize(key)}: ${data[key]}`);
-  return `Ukensmeny på ${sted}: ${meny}`;
+  return `Denne ukens meny på ${sted}: ${meny}`;
 };
 
 const dagsmeny = (data, sted) => `Dagens meny på ${sted}: ${data}`;
 
 const menyMangler = sted => `Ingen meny tilgjengelig på ${sted} :pensive:`;
 
-const getDagsmeny = (sted, ukedag) => (
-  menyer[sted] ? ukemeny(menyer[sted], sted) : menyMangler(sted)
-);
+const getDagsmeny = (sted, ukedag) => {
+  const data = menyer[sted];
+  return data ? dagsmeny(data[ukedag], sted) : menyMangler(sted);
+};
 
-const getUkemeny = (sted) => (
-  menyer[sted] ? ukemeny(menyer[sted], sted) : menyMangler(sted)
-);
+const getUkemeny = (sted) => {
+  const data = menyer[sted];
+  return data ? ukemeny(data, sted) : menyMangler(sted);
+};
 
-const getVarsel = (robot, rom) => {
+const getVarsel = (robot, rom) => (
   new CronJob({
     cronTime: '00 30 10 * * 1-5',
     onTick: () => {
@@ -50,30 +55,38 @@ const getVarsel = (robot, rom) => {
       const menyHuset = getDagsmeny('huset', ukedag);
       const menyGalleriet = getDagsmeny('galleriet', ukedag);
 
-      robot.messageRoom(rom, `Nå er det snart tid for lunsj! :hamburger: \n${menyHuset}\n${menyGalleriet}`);
+      robot.messageRoom(rom, `Det er straks tid for lunsj! :hamburger: \n${menyHuset}\n${menyGalleriet}`);
     },
     start: true,
     timeZone: 'Europe/Oslo'
   })
-};
+);
 
 const aktiverVarsel = (robot, rom) => {
   lunsjvarsler[rom] = getVarsel(robot, rom);
 };
 
 const aktiverVarsler = (robot) => {
-  const romListe = menyer.rom;
+  const romliste = menyer.rom;
   const aktiverteVarsler = Object.keys(lunsjvarsler);
-
-  romListe.filter(rom => aktiverteVarsler.indexOf(rom) === -1)
+  romliste.filter(rom => aktiverteVarsler.indexOf(rom) === -1)
     .forEach((rom) => {
       aktiverVarsel(robot, rom);
     });
 };
 
 module.exports = (robot) => {
-  aktiverVarsel(robot);
-  robot.respond(/(lunsj|lunch|lønsj) ((?:i ?|man|tirs|ons|tors|fre)dag) ?(galleriet|huset)?(:vårt)?$/i, (res) => {
+  aktiverVarsler(robot);
+
+  robot.respond(/(lunsj|lunch|lønsj)$/i, (res) => {
+    logRespond(robot, res);
+    const menyHuset = getUkemeny('huset');
+    const menyGalleriet = getUkemeny('galleriet');
+
+    res.send(`${menyHuset}\n${menyGalleriet}`);
+  });
+
+  robot.respond(/(lunsj|lunch|lønsj) ((?:i ?|man|tirs|ons|tors|fre)dag) ?(galleriet|huset)?(?: vårt)?$/i, (res) => {
     logRespond(robot, res);
     const dag = res.match[2] === 'idag' || res.match[2] === 'i dag' ?
       new Date().getDay() - 1 :
@@ -98,5 +111,5 @@ module.exports = (robot) => {
     logRespond(robot, res);
     const sted = res.match[1];
     res.send(getUkemeny(sted));
-  })
+  });
 };

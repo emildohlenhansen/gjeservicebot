bb80de9f4bb8a83b661b3d7ef1a1ab3143687ba3
Håkon Meyer Tørnquist <haakon.t@gmail.com>
2018-10-08 13:23:09 +0200

    Update bursdager.js
    La til de bursdagene jeg kunne finne ut av på stående fot og gav de som var der litt penere navn 🌮 Kan se om jeg finner ut av de med spørsmålstegn også, men noen er flinkere til å gjemme ting på internett enn andre

--- static/bursdager.js
+++ static/bursdager.js
@@ -1,9 +1,17 @@
 module.exports = {
   bursdager: {
-    "Martin Sangolt": "27/05/1992",
-    "mfeiring": "29/04/1990",
-    "sigurdfalken": "20/04/1989",
-    "emildh": "27/01/1992",
+    "Martin": "27/05/1992",
+    "Mira": "29/04/1990",
+    "Sigurd": "20/04/1989",
+    "Emil": "27/01/1992",
+    "Julij": "14/05/1986",
+    "Håkon": "13/03/1992",
+    "Jon Magnus": "28/05/1993",
+    "Fredrik": "28/08/1991",
+    "Thomas": "13/10/????",
+    "Eivind": "30/10/????",
+    "Pål": "09/12/1986",
+    "Thinh": "??/??/1990",
   },
   rom: [
     'CD1HHGMC0'

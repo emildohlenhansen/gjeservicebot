725cbc3912829ecd328913f246ca3b7004cacf35
Emil Døhlen Hansen <emil.dohlen.hansen@bekk.no>
2018-10-10 10:53:42 +0200

    Update statusansvarlig.js

--- scripts/statusansvarlig.js
+++ scripts/statusansvarlig.js
@@ -4,7 +4,7 @@ const CronJob = require('cron').CronJob;
 const statusansvarlige = require('../static/statusansvarlig');
 
 const getAnsvarliglisteliste = () => {
-  const ansvarligliste = statusansvarlige.ansvarlige.map(([id, uke]) => `\n\t${uke}: <@${id}>`);
+  const ansvarligliste = statusansvarlige.ansvarlige.map(([id, uke]) => `\n\t Uke: ${uke}: <@${id}>`);
   return `Ansvarlige for overvåkning av våre tjenester: ${ansvarligliste}`;
 };
 

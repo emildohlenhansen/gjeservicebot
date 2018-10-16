2024dc25f5aec4f46e37c2956bad95897d51e90b
emildohlenhansen <emil.dohlen.hansen@bekk.no>
2018-10-16 16:03:20 +0200

    update statusansvarlig

--- scripts/statusansvarlig.js
+++ scripts/statusansvarlig.js
@@ -3,10 +3,24 @@ const CronJob = require('cron').CronJob;
 
 const statusansvarlige = require('../static/statusansvarlig');
 
-const getAnsvarliglisteliste = () => {
+const getAnsvarliglistelisteOversikt = () => {
   const ansvarligliste = statusansvarlige.ansvarlige.map(([id, uke]) => `\n\t Uke: ${uke}: <@${id}>`);
   return `Ansvarlige for overvåkning av våre tjenester: ${ansvarligliste}`;
 };
+
+const getAnsvarliglisteliste = () => {
+  const ansvarligliste = statusansvarlige.ansvarlige;
+  for (let index in ansvarligliste) {
+    const forrigeHvem = index > 0 ? ansvarligliste[index - 1][0] : "Ingen";
+    const hvem = ansvarligliste[index][0];
+    const uke = ansvarligliste[index][1];
+
+    if (moment().week(uke).isSame(moment(), 'week')) {
+      return `:bell: Denne uka er <@${hvem}> ansvarlig for overvåkning av våre tjenster :tv:`;
+    }
+  }
+};
+
 
 aktiverAnsvarligliste = () => {
   new CronJob({
@@ -14,12 +28,12 @@ aktiverAnsvarligliste = () => {
     onTick: () => {
       const ansvarligliste = statusansvarlige.ansvarlige;
       const rom = statusansvarlige.rom;
-      for (const index in ansvarligliste) {
-        const forrigeHvem = ansvarligliste[index - 1][0];
+      for (let index in ansvarligliste) {
+        const forrigeHvem = index > 0 ? ansvarligliste[index - 1][0] : "Ingen";
         const hvem = ansvarligliste[index][0];
         const uke = ansvarligliste[index][1];
 
-        if (moment.week(uke).isSame(moment(), 'week')) {
+        if (moment().week(uke).isSame(moment(), 'week')) {
           robot.messageRoom(rom, `:bell: Denne uka er <@${hvem}> ansvarlig for overvåkning av våre tjenster :tv:. <@${forrigeHvem}> holder oppsummering om forrige uke på mandagsmøtet :ok_hand:.`);
         }
       }
@@ -31,8 +45,13 @@ aktiverAnsvarligliste = () => {
 
 module.exports = (robot) => {
   aktiverAnsvarligliste(robot);
-  robot.respond(/(statusansvarlig)$/i, (res) => {
-    const ansvarligliste = getAnsvarliglisteliste();
-    res.send(`${ansvarligliste}`);
+  robot.respond(/(statusansvarligoversikt)$/i, (res) => {
+    const ansvarliglisteOversikt = getAnsvarliglistelisteOversikt();
+    res.send(`${ansvarliglisteOversikt}`);
+
+    robot.respond(/(statusansvarlig)$/i, (res) => {
+      const ansvarligliste = getAnsvarliglisteliste();
+      res.send(`${ansvarligliste}`);
+    });
   });
 };
--- static/statusansvarlig.js
+++ static/statusansvarlig.js
@@ -2,13 +2,13 @@ module.exports = {
   ansvarlige: [
     ["ingen", "40"], // Sigurd
     ["UA26TSN1Z", "41"], // Sigurd
-    ["UA4MGB1RA", "42"], // Eivind
-    ["UC30379K6", "43"], // Bent
-    ["UB9CWUCS1", "44"], // Emil
-    ["UA52Y6GE9", "45"], // Fredrik
-    ["UA5QWQ9BQ", "46"], // Håkon
-    ["UBECSNGTA", "47"], // Mira
-    ["UA4U8PN74", "48"], // Jon Magnus
+    ["UA4U8PN74", "42"], // Jon Magnus
+    ["UA4MGB1RA", "43"], // Eivind
+    ["UC30379K6", "44"], // Bent
+    ["UB9CWUCS1", "45"], // Emil
+    ["UA52Y6GE9", "46"], // Fredrik
+    ["UA5QWQ9BQ", "47"], // Håkon
+    ["UBECSNGTA", "48"], // Mira
     ["UA536664V", "49"] // Pål
   ],
   rom: [

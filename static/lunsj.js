5adce013b4a8df5ef76873fdf410adc711fab46f
Emil Døhlen Hansen <emil.dohlen.hansen@bekk.no>
2018-10-22 09:15:40 +0200

    Update lunsj.js

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,17 +1,17 @@
 module.exports = {
   huset: {
-    mandag: "BBQ kyllingklubber, Seibiff med løk, Blomkål og brokkolisuppe",
-    tirsdag: "Pasta penne arrabiata, Torsk med smørsaus, Fiskesuppe",
-    onsdag: "Flesk og duppe, Paella, Tomatsuppe",
-    torsdag: "Fårikål, Laksewok, Tom Yum Gai suppe",
-    fredag: "Ølmarinert svinekjake, Dagens suppe"
+    mandag: "Pasta med kjøttsaus :spaghetti:, Torsk med topping, Soppsuppe",
+    tirsdag: "Grillet kyllinglår",
+    onsdag: "Stekt laks",
+    torsdag: "Clubsandwich",
+    fredag: "Hamburger :hamburger:"
   },
   galleriet: {
-    mandag: "Vegemite med tzatziki, Grønnsakssuppe",
-    tirsdag: "Kylling med couscous, Oksehalesuppe",
-    onsdag: "Laks med agurksalat, Kyllingsuppe",
-    torsdag: "Bolognese, Soppsupe",
-    fredag: "Pølsefest"
+    mandag: "Kyllinglår med curry og ris, Grønnsakssuppe",
+    tirsdag: "Laks med salat, Blomkålsuppe",
+    onsdag: "Wienerschnitzel med rødkål, Tomatsuppe",
+    torsdag: "Torsk med ertepure og bacon, Minestronesuppe",
+    fredag: "Kjøttkaker med mos"
   },
   rom: [
     'CA22ZT7TN'

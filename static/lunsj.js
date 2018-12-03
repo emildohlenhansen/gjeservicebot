1365d655a710699bef9c4aa822ad780e4c0efe82
Mira Thoen Feiring <mfeiring@users.noreply.github.com>
2018-12-03 11:36:46 +0100

    Update lunsj.js

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,17 +1,17 @@
 module.exports = {
   huset: {
-    mandag: "Pasta med kjøttsaus, Seibiff med løk, Aspargessuppe",
-    tirsdag: "Grønnsakscurry med kikerter, Klippfiskburger på grovbrød, Tom Yum Gaisuppe",
-    onsdag: "Fersk kjøtt med løksaus, Søtpotet og linsesuppe",
-    torsdag: "Lammegryte, Laksewok, Fiskesuppe",
-    fredag: "Fish and chips :fish::fries:"
+    mandag: "Kyllinggryte, Dampet torsk med smørsaus, Soppsuppe med tilbehør og brød",
+    tirsdag: "Svinesnitzel, Kryddersild med tilbehør, Argentinsk bønne og linsesuppe med tilbehør og brød",
+    onsdag: "Tortellini med ricotta og spinat, Stekt laks med bulgut og bakte cherrytomater, Ertesuppe med tilbehør og flatbrød",
+    torsdag: "Dansk hakkebøff med løk og fløtesaus, Dampet uerfilet, Blomkålsuppe med bacon, tilbehør og brød",
+    fredag: "Kyllingkebab med tilbehør, Dagens fisk med tilbehør, Fiskesuppe med tilbehør og brød"
   },
   galleriet: {
-    mandag: "Pasta Primavera, Grønnsakssuppe",
-    tirsdag: "chili con carne, Søtpotet med kokos",
-    onsdag: "torsk med pepperrot smør, Kyllingsuppe",
-    torsdag: "Kylling med stekte poteter og pesto, Tomatsuppe",
-    fredag: "Taco fest"
+    mandag: "Stekt Kyllinglår med soppsaus, Grønnsakssuppe",
+    tirsdag: "Kjøttkaker med ertepuré, Sellerirotsuppe",
+    onsdag: "Laks med Beurre Blanc Saus, Soppsuppe",
+    torsdag: "Kremet pasta med bacon og pølser, Blomkålsuppe",
+    fredag: "Kjøttboller med Potetmos"
   },
   rom: [
     'CA22ZT7TN'

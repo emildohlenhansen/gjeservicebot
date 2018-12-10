b95490ff2cfbaf8ef188a4f08d17aa16f281399c
Emil Døhlen Hansen <emil.dohlen.hansen@bekk.no>
2018-12-10 08:37:58 +0100

    update lunsj.js

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,17 +1,17 @@
 module.exports = {
   huset: {
-    mandag: "Kyllinggryte, Dampet torsk med smørsaus, Soppsuppe med tilbehør og brød",
-    tirsdag: "Svinesnitzel, Kryddersild med tilbehør, Argentinsk bønne og linsesuppe med tilbehør og brød",
-    onsdag: "Tortellini med ricotta og spinat, Stekt laks med bulgut og bakte cherrytomater, Ertesuppe med tilbehør og flatbrød",
-    torsdag: "Dansk hakkebøff med løk og fløtesaus, Dampet uerfilet, Blomkålsuppe med bacon, tilbehør og brød",
-    fredag: "Kyllingkebab med tilbehør, Dagens fisk med tilbehør, Fiskesuppe med tilbehør og brød"
+    mandag: "Asiatisk lammegryte, Torsk med topping, Kremet grønnsakssuppe",
+    tirsdag: "Indisk linsegryte dal, Laksewok, Løksuppe",
+    onsdag: "Røkt svinekam, Stekt røye, Fiskesuppe",
+    torsdag: "Kyllingfajitas, Sprøbakt hoki, Meksikansk tomatsuppe",
+    fredag: "Juletallerken :santa:"
   },
   galleriet: {
-    mandag: "Stekt Kyllinglår med soppsaus, Grønnsakssuppe",
-    tirsdag: "Kjøttkaker med ertepuré, Sellerirotsuppe",
-    onsdag: "Laks med Beurre Blanc Saus, Soppsuppe",
-    torsdag: "Kremet pasta med bacon og pølser, Blomkålsuppe",
-    fredag: "Kjøttboller med Potetmos"
+    mandag: "Røkte Svinekoteletter med potetmos, Grønnsakssuppe",
+    tirsdag: "Stekte kyllinglår med tzatzitki, Fiskesuppe",
+    onsdag: "Fiskekaker med brunsaus, Thai kyllingsuppe",
+    torsdag: "Kjøttgryte med ris, Risgrøt",
+    fredag: "Pølsefest"
   },
   rom: [
     'CA22ZT7TN'

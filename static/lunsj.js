59f0fbef78fe259983150b1e25d6172bbb74971a
Emil Døhlen Hansen <emil.dohlen.hansen@bekk.no>
2019-08-27 10:06:34 +0200

    Lunsjmeny uke 35

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,17 +1,17 @@
 module.exports = {
  huset: {
-    mandag: "Pasta med skinke. Dampet torsk med sitronsaus. Aspargessuppe ",
-    tirsdag: "Boboli. Sprøbakt sei. Meksikansk tomatsuppe",
-    onsdag: "Linsegryte med raita. Sirdalsørret med agurksalat, rømme og kokte poteter. Fiskesuppe",
-    torsdag: "Moussaka. Panert rødspette med remulade. Indisk linsesuppe",
-    fredag: "Biffsnadder med pommes frites, bearnaisesaus og salat. Dagens suppe"
+    mandag: "Kyllingwok, Seibiff med løk. Tilbehør: Bulgur, Wokgrønnsakers, Fiskesuppe med tilbehør og brød",
+    tirsdag: "Chili con carne, Torsk med topping. Tilbehør: Stekte Folvapoteter, Lun paprika og fennikelsalat, Tom Yum Gai suppe med tilbehør og brød",
+    onsdag: "Grønnsakslasagne, Green curry fiskegryte. Tilbehør: Ris, Salat, Blomkål og brokkolisuppe med tilbehør og brød",
+    torsdag: "Raspeball med salt kjøtt, vossakorv og rotmos, Dagens fisk med tilbehør, Gresskarsuppe med tilbehør og brød",
+    fredag: "Kylling og lammekebab med salat og dressinger, Dagens suppe med tilbehør og brød"
   },
   galleriet: {
-    mandag: "Kyllinglår med pasta",
-    tirsdag: "Svinebog med rødvinsaus",
-    onsdag: "Laks med hollandaise og grønnkål",
-    torsdag: "Risotto med svin indrefilèt og kantarell",
-    fredag: "Gresk pai med salat"
+    mandag: "Rød karrigryte med kyllingkjøtt",
+    tirsdag: "Pytt i panne",
+    onsdag: "Fiskegrateng",
+    torsdag: "Marinert svinenakke",
+    fredag: "Fredagspizza"
   },
   rom: [
     'CA22ZT7TN'

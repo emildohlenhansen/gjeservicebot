ac6a78a045df7d16c5652ceab9018db1f9339cb9
Emil Døhlen Hansen <emil.dohlen.hansen@bekk.no>
2019-09-02 09:53:07 +0200

    update lunsjmeny

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,13 +1,13 @@
 module.exports = {
  huset: {
-    mandag: "Kyllingwok, Seibiff med løk. Tilbehør: Bulgur, Wokgrønnsakers, Fiskesuppe med tilbehør og brød",
+    mandag: "Ingen meny :thinking_face:",
     tirsdag: "Chili con carne, Torsk med topping. Tilbehør: Stekte Folvapoteter, Lun paprika og fennikelsalat, Tom Yum Gai suppe med tilbehør og brød",
     onsdag: "Grønnsakslasagne, Green curry fiskegryte. Tilbehør: Ris, Salat, Blomkål og brokkolisuppe med tilbehør og brød",
     torsdag: "Raspeball med salt kjøtt, vossakorv og rotmos, Dagens fisk med tilbehør, Gresskarsuppe med tilbehør og brød",
     fredag: "Kylling og lammekebab med salat og dressinger, Dagens suppe med tilbehør og brød"
   },
   galleriet: {
-    mandag: "Rød karrigryte med kyllingkjøtt",
+    mandag: "Ingen meny :thinking_face:",
     tirsdag: "Pytt i panne",
     onsdag: "Fiskegrateng",
     torsdag: "Marinert svinenakke",

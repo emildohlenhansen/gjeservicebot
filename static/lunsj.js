c4202b7ffb12164fdc9d07a9cddc05af85e170ea
Emil Døhlen Hansen <emil.dohlen.hansen@bekk.no>
2019-09-09 09:26:51 +0200

    lunsj uke 37

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,17 +1,17 @@
 module.exports = {
  huset: {
-    mandag: "Ingen meny :thinking_face:",
-    tirsdag: "Kremet tortellini med ricotta og spinat, Fiskeburger på grovbrød, Fiskesuppe",
-    onsdag: "Børek, Klippfiskgryte, Rødbetssuppe",
-    torsdag: "Pytt i panne med speilegg, Paella, Kyllingsuppe",
-    fredag: "Tortilla med salat, hjemmelaget salsa og rømme"
+    mandag: "Kyllingwok, Dampet torsk med spinat og selleristuing, Fiskesuppe",
+    tirsdag: "Jambalaya, Sprøbakt sei, Tom Yum Gai suppe",
+    onsdag: "Aloo Gobi, Pasta med laks, Kremet grønnsakssuppe",
+    torsdag: "Kjøttkaker, Thailandsk kveitegryte, Løksuppe",
+    fredag: "Langtidsstekt høyrygg av urfe med saltbakte mandelpoteter og blomkål/ brokkoligrateng"
   },
   galleriet: {
-    mandag: "Ghana masala med kylling, nan og ris, Grillet paprika med koriander",
-    tirsdag: "Lemongrass beef med nudler, Brokkolisuppe",
-    onsdag: "Pasta med røkt laks, Russisk solyanka",
-    torsdag: "Kyllinglår med sursøt saus, Blomkålsuppe",
-    fredag: "Fredagsburger, Gul ertesuppe"
+    mandag: "Gresk Lemonato Kylling, Betasuppe",
+    tirsdag: "Fiskekaker med fløtepoteter, Kjøttsuppe",
+    onsdag: "Schnitzel med stekte poteter, Gulrotsuppe",
+    torsdag: "Pasta med biffkjøtt og skogsopp saus, Muligatowny suppe",
+    fredag: "Kylling Quesadillas, Thaisuppe"
   },
   rom: [
     'CA22ZT7TN'

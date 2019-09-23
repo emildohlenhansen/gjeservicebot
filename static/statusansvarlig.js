d5988340deea8950f380b772e9ae35b3fa28847e
Emil Døhlen Hansen <emil.dohlen.hansen@bekk.no>
2019-09-23 09:05:44 +0200

    update statusansvarlig

--- static/statusansvarlig.js
+++ static/statusansvarlig.js
@@ -3,14 +3,14 @@ module.exports = {
     ["ingen", "12"], // Sigurd
     ["UA26TSN1Z", "35"], // Sigurd
     ["UA4U8PN74", "36"], // Jon Magnus
-    ["UA4MGB1RA", "38"], // Eivind
     ["UC30379K6", "37"], // Bent
+    ["UA4MGB1RA", "38"], // Eivind
     ["UA52Y6GE9", "39"], // Fredrik
     ["UB9CWUCS1", "40"], // Emil
     ["UA5QWQ9BQ", "41"], // Håkon
-    ["UBECSNGTA", "44"], // Mira
-    ["UEXRX3QKX", "43"], // Edvard
     ["UM7MLBFC3", "42"], // Hege
+    ["UEXRX3QKX", "43"], // Edvard
+    ["UBECSNGTA", "44"], // Mira
     ["UMELX0XAR", "45"] // Martin
   ],
   rom: [

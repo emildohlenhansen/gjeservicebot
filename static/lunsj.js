988da6d3bdc6eaafa5d154cf30c7974ebc6aa329
Emil Døhlen Hansen <emil.dohlen.hansen@bekk.no>
2019-10-07 10:27:26 +0200

    uke 41

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,17 +1,17 @@
 module.exports = {
  huset: {
-    mandag: "Grillet kyllinglår, Seibiff med løk, Indisk potet og blomkålsuppe",
-    tirsdag: "Sheppards pai med grov potetmos, Laksewok, Kyllingsuppe",
-    onsdag: "Hønsefrikassé med kokte poteter og grønnsaker, Paella, Løksuppe",
-    torsdag: "Kremet tortellini med ricotta og spinat, Dampet torsk med skalldyrsaus, bulgur og lun tomatsalat, Fiskesuppe",
-    fredag: "Får ytrefilet med fløtegratinerte poteter og sesongens grønnsaker"
+    mandag: "Stroganoffgryte, Pankopanert rødspette, Potet og purreløksuppe med tilbehør og brød",
+    tirsdag: "Boboli, Kilppfiskgryte, Brokkolisuppe med kokos, tilbehør og brød",
+    onsdag: "Flesk og duppe, Dampet torsk med hollandaisesaus, Fiskesuppe med tilbehør og brød",
+    torsdag: "Spanske kjøttboller med byggrynsrisotto og bakte grønnsaker, Pasta med laks, Meksikansk maissuppe med tilbehør og brød",
+    fredag: "Libabrød med spicy kylling/ lammekjøttdeig og salat, Dagens fisk med tilbehør, Dagens suppe med tilbehør og brød"
   },
   galleriet: {
-    mandag: "Boeuf Stroganow med ris, Pølsesuppe",
-    tirsdag: "Husets kjøttkakker med brun saus og erter stuing, Thaisuppe",
-    onsdag: "Ørret, Fransk løksuppe",
-    torsdag: "Kålruller med kyllingkjøtt og ris, Meksikansk bønner suppe",
-    fredag: "Gresk gyros med brød tzatziki og salat"
+    mandag: "Kalvegryte med sopp, Gulrotsuppe med kokosmelk",
+    tirsdag: "Chilli mango svinekjøtt med ris, Blomkålsuppe",
+    onsdag: "Panert torsk, Gulasjsuppe",
+    torsdag: "Kumle med saltet kjøtt og poteter, Tandori kyllingsuppe",
+    fredag: "Burritos"
   },
   rom: [
     'CA22ZT7TN'

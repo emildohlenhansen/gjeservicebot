174fb461f4ac10434784e97962d9fc89790a24d4
Emil Døhlen Hansen <emil.dohlen.hansen@bekk.no>
2019-10-21 10:04:23 +0200

    uke 43

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,17 +1,17 @@
 module.exports = {
  huset: {
-    mandag: "Pasta med kjøttsaus, Dampet torsk med baconsmør, ertestuing og kokt potet, Soppsuppe",
+    mandag: "Ingen meny :pensive:",
     tirsdag: "Grønnsakslasagne,Sprøbakt hysefilet med råkost og bulgur, Kremet rødbetssuppe",
     onsdag: "Komle med vossakorv, kålrabbistappe og potet, Laksewok, Tomatsuppe",
     torsdag: "Egg og bacon med tomatbønner og stekte poteter, Fiskeburger, Fiskesuppe",
     fredag: "Hvalgryte med grønnsaker og kokt potet"
   },
   galleriet: {
-    mandag: "Tortellini med spinat og ricotta kylling pesto saus, Purreløk og poteter suppe",
-    tirsdag: "Lammefrikasse, Søtpotet med chorizo",
-    onsdag: "Fiskekakker med råkost, Bønnesuppe",
-    torsdag: "Risotto med sopp og kylling lår, Betasuppe",
-    fredag: "Egg nudler med sitron gres biff, Masala indisk med linser"
+    mandag: "Hjortekjøtt gryte, Hvit aspargessuppe",
+    tirsdag: "Stekt svinenakke med kål og poteter, soppsuppe",
+    onsdag: "Torsk med tilbehør, Spansk chorizo med linser",
+    torsdag: "Cotoletta milanese med potet purre, Tomatsuppe med basilikum",
+    fredag: "Fredag’s Burger, Thaisuppe"
   },
   rom: [
     'CA22ZT7TN'

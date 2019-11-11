0725ee1046cbd46d4522bdb28f235194f0508a46
Emil Døhlen Hansen <emil.dohlen.hansen@bekk.no>
2019-11-11 14:35:04 +0100

    update lunsj.js

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,10 +1,10 @@
 module.exports = {
  huset: {
     mandag: "Wok med sesammarinerte biffstrimler og friske grønnsaker, Torsk med ostetopping, friske grønnsaker og couscous, Fiskesuppe med tilbehør og brød",
-    tirsdag: "Kremet tortellini med ricotta og spina, Lettsaltet brosme, ris og spinat/selleripuré, Spisestuas ertesuppe med flatbrød",
+    tirsdag: "Kremet tortellini med ricotta og spinat, Lettsaltet brosme, ris og spinat/selleripuré, Spisestuas ertesuppe med flatbrød",
     onsdag: "Kjøttgryte med potetmos og dampede grønnsaker fra Huseby gård, Paella, Kyllingsuppe med tilbehør og brød",
-    torsdag: "Libabrød med shawarmakrydret kjøttdeig og lun paprikasalat, Fisk i skjell, Spinatsuppe med tilbehør og brød",
-    fredag: "Gresk sitronkylling med stekte poteter og tomatsalat, Dagens fisk med tilbehør, Dagens suppe med tilbehør og brød"
+    torsdag: "Libabrød med shawarmakrydret kjøttdeig og lun paprikasalat, Fisk i skjell,  Spinatsuppe med tilbehør og brød",
+    fredag: "Gresk sitronkylling med stekte poteter og tomatsalat, Dagens fisk med tilbehør,  Dagens suppe med tilbehør og brød"
   },
   galleriet: {
     mandag: "Jamiacan Jerk kylling med ris, Mulligatawny suppe",

124db1970f865db6d07036b5d2e5485eaeb2c7db
Martin Wahlberg <martinwahlberg@icloud.com>
2019-11-11 09:50:49 +0100

    Uke 46

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,17 +1,17 @@
 module.exports = {
  huset: {
-    mandag: "Ingen meny :pensive:",
-    tirsdag: "Ingen meny :pensive:",
-    onsdag: "Ingen meny :pensive:",
-    torsdag: "Ingen meny :pensive:",
-    fredag: "Ingen meny :pensive:",
+    mandag: "Wok med sesammarinerte biffstrimler og friske grønnsaker, Torsk med ostetopping, friske grønnsaker og couscous, Fiskesuppe med tilbehør og brød",
+    tirsdag: "Kremet tortellini med ricotta og spina, Lettsaltet brosme, ris og spinat/selleripuré, Spisestuas ertesuppe med flatbrød",
+    onsdag: "Kjøttgryte med potetmos og dampede grønnsaker fra Huseby gård, Paella, Kyllingsuppe med tilbehør og brød",
+    torsdag: "Libabrød med shawarmakrydret kjøttdeig og lun paprikasalat, Fisk i skjell, Spinatsuppe med tilbehør og brød",
+    fredag: "Gresk sitronkylling med stekte poteter og tomatsalat, Dagens fisk med tilbehør, Dagens suppe med tilbehør og brød",
   },
   galleriet: {
-    mandag: "Hjortekjøtt gryte, Hvit aspargessuppe",
-    tirsdag: "Stekt svinenakke med kål og poteter, soppsuppe",
-    onsdag: "Torsk med tilbehør, Spansk chorizo med linser",
-    torsdag: "Cotoletta milanese med potet purre, Tomatsuppe med basilikum",
-    fredag: "Fredag’s Burger, Thaisuppe"
+    mandag: "Jamiacan Jerk kylling med ris, Mulligatawny suppe",
+    tirsdag: "Moussakka med salat, Ungarsk gulasjsuppe",
+    onsdag: "Fiskegrateng med råkost, Tom Kha Gai",
+    torsdag: "Chilli mango svinewok, Sopp suppe",
+    fredag: "Nachos, Bakt paprika suppe"
   },
   rom: [
     'CA22ZT7TN'

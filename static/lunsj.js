70d4e5735ef85490047371200e7a9b44abaf70d2
Martin Wahlberg <martinwahlberg@icloud.com>
2019-11-18 09:39:27 +0100

    Update lunsj.js

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,17 +1,17 @@
 module.exports = {
  huset: {
-    mandag: "Wok med sesammarinerte biffstrimler og friske grønnsaker, Torsk med ostetopping, friske grønnsaker og couscous, Fiskesuppe med tilbehør og brød",
-    tirsdag: "Kremet tortellini med ricotta og spinat, Lettsaltet brosme, ris og spinat/selleripuré, Spisestuas ertesuppe med flatbrød",
-    onsdag: "Kjøttgryte med potetmos og dampede grønnsaker fra Huseby gård, Paella, Kyllingsuppe med tilbehør og brød",
-    torsdag: "Libabrød med shawarmakrydret kjøttdeig og lun paprikasalat, Fisk i skjell,  Spinatsuppe med tilbehør og brød",
-    fredag: "Gresk sitronkylling med stekte poteter og tomatsalat, Dagens fisk med tilbehør,  Dagens suppe med tilbehør og brød"
+    mandag: "Pasta med kylling og friske grønnsaker, Dampet torsk med baconsmør, ertestuing og kokte poteter, Indisk linsesuppe med tilbehør og brød",
+    tirsdag: "Boboli, Sprøbakt brosme med råkost og couscous, Tomatsuppe med tilbehør og brød",
+    onsdag: "Erter, saltkjøtt og flesk med dampede poteter fra Vestfold og kokte rotgrønnsaker, Skalldyrwok,  Blomkål og brokkolisuppe med tilbehør og brød",
+    torsdag: "Asiatisk svinegryte med ris og bakte tomater, Laskelasagne med salat, Gulrotsuppe med tilbehør og brød",
+    fredag: "Tortilla med Spisestuas salsa, rømme og salat, Dagens fisk med tilbehør,  Dagens suppe med tilbehør og brød"
   },
   galleriet: {
-    mandag: "Jamiacan Jerk kylling med ris, Mulligatawny suppe",
-    tirsdag: "Moussakka med salat, Ungarsk gulasjsuppe",
-    onsdag: "Fiskegrateng med råkost, Tom Kha Gai",
-    torsdag: "Chilli mango svinewok, Sopp suppe",
-    fredag: "Nachos, Bakt paprika suppe"
+    mandag: "Chili con polo med ris, Marokansk kjøttsuppe",
+    tirsdag: "Biff bordolaise pasta, Tex mex suppe",
+    onsdag: "FTeriyaki laks med grønt, Frikadelessuppe",
+    torsdag: "Cajun kylling med bakte poteter, Linsesuppe",
+    fredag: "Rokt grov currywurst med potet og lål salat, Brokkolisuppe"
   },
   rom: [
     'CA22ZT7TN'

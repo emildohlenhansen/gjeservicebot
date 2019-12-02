05397d40c3d507b7e0d9b8a0be2178295be45f72
Hege <hegehaavaldsen@gmail.com>
2019-12-02 12:33:17 +0100

    Add firebase project for standup

--- scripts/standup.js
+++ scripts/standup.js
@@ -1,6 +1,22 @@
 //const CronJob = require("cron").CronJob;
 
-const baseUrl = "https://slack.com/api/";
+const firebase = require("firebase/app");
+
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBsHrSqz3wyLA0uoD9kz4CBTECcyII-o78",
+  authDomain: "servicebot-standup.firebaseapp.com",
+  databaseURL: "https://servicebot-standup.firebaseio.com",
+  projectId: "servicebot-standup",
+  storageBucket: "servicebot-standup.appspot.com",
+  messagingSenderId: "71766126845",
+  appId: "1:71766126845:web:703cd92be1f5e828118e94"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 module.exports = robot => {
   robot.respond(/cron/, _ => {
@@ -26,7 +42,23 @@ module.exports = robot => {
 
   robot.respond(/standup (.*) (.*) (.*)/, res => {
     const [yesterday, today, obstacles] = res.match;
-    console.log();
+
+    db.collection("standup")
+      .doc(Date.now())
+      .set({
+        date: Date.now(),
+        user: res.envelope.user.id,
+        yesterday,
+        today,
+        obstacles
+      })
+      .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+
     res.reply(
       `:champagne: ${yesterday}\n:rocket: ${today}\n:boom: ${obstacles}\n`
     );

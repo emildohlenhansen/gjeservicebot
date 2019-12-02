2264ff45b63271f95ac1d84e07486ed36dc9ba0f
Hege <hegehaavaldsen@gmail.com>
2019-12-02 10:14:45 +0100

    Try list all channels

--- scripts/standup.js
+++ scripts/standup.js
@@ -3,10 +3,15 @@ const CronJob = require("cron").CronJob;
 const baseUrl = "https://slack.com/api/";
 
 module.exports = robot => {
-  robot.respond(/channels/, res => {
-    const channels = robot.http(
-      `${baseUrl}channels.list?token=${process.env.HUBOT_SLACK_TOKEN}`
-    );
-    res.send(channels);
+  robot.respond(/standup/, msg => {
+    robot
+      .http(
+        `https://slack.com/api/channels.list?token=${process.env.HUBOT_SLACK_TOKEN}&exclude_archived=1`
+      )
+      .get()((err, res, body) => {
+      const channels = JSON.parse(body);
+
+      msg.send(`${channels}`);
+    });
   });
 };

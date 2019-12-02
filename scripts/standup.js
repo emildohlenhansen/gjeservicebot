44b2d98d8cb52964df0df4097ebd97cdfa1758bf
emildohlenhansen <emil.dohlen.hansen@bekk.no>
2019-12-02 14:17:21 +0100

    remove name

--- scripts/standup.js
+++ scripts/standup.js
@@ -1,29 +1,26 @@
-//const CronJob = require("cron").CronJob;
+// const CronJob = require("cron").CronJob;
 
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 
 const serviceAccount = process.env.STANDUP_PK;
 
-admin.initializeApp(
-  {
-    credential: admin.credential.cert(JSON.parse(serviceAccount))
-  },
-  "standup"
-);
+admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(serviceAccount))
+});
 
 const db = admin.firestore();
 
-module.exports = robot => {
-  robot.respond(/cron/, _ => {
+module.exports = (robot) => {
+  robot.respond(/cron/, (_) => {
     robot
       .http(
         `https://slack.com/api/channels.list?token=${process.env.HUBOT_SLACK_TOKEN}&exclude_archived=1`
       )
       .get()((err, res, body) => {
-      const channels = JSON.parse(body);
-      if (channels.ok) {
-        channels.channels
-          .filter(c => c.name === "gjeservicebot-gje-workshop")
+        const channels = JSON.parse(body);
+        if (channels.ok) {
+          channels.channels
+          .filter(c => c.name === 'gjeservicebot-gje-workshop')
           .map(c => c.members)[0]
           .forEach(id =>
             robot.messageRoom(
@@ -31,25 +28,23 @@ module.exports = robot => {
               'Reminder: Standup! :loudspeaker:\n\n \n\nFormat: _standup_ "hva du gjorde i går" "hva du skal gjøre i dag" "hvilke hindringer du har"'
             )
           );
-      }
-    });
+        }
+      });
   });
 
-  robot.respond(/standup (.*) (.*) (.*)/, res => {
+  robot.respond(/standup (.*) (.*) (.*)/, (res) => {
     const [yesterday, today, obstacles] = res.match;
 
-    db.collection("users")
-    .doc(res.envelope.user.id)
-    .collection("standups")
-    .add({
-      date: Date.now(),
-      yesterday,
-      today,
-      obstacles
-    });
+    db.collection('users')
+      .doc(res.envelope.user.id)
+      .collection('standups')
+      .add({
+        date: Date.now(),
+        yesterday,
+        today,
+        obstacles
+      });
 
-    res.reply(
-      `:champagne: ${yesterday}\n:rocket: ${today}\n:boom: ${obstacles}\n`
-    );
+    res.reply(`:champagne: ${yesterday}\n:rocket: ${today}\n:boom: ${obstacles}\n`);
   });
 };

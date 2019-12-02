112c879927f1f8563685949582829a21b79c51c8
Bent Aksel Langlo Rynning <Bent-Aksel-Langlo.Rynning@devgjensidige.com>
2019-12-02 14:37:29 +0100

    Test

--- scripts/standup.js
+++ scripts/standup.js
@@ -1,29 +1,26 @@
 // const CronJob = require("cron").CronJob;
 
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 const serviceAccount = process.env.STANDUP_PK;
 
-admin.initializeApp(
-  {
-    credential: admin.credential.cert(JSON.parse(serviceAccount))
-  },
-  'standup'
-);
+admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(serviceAccount))
+});
 
 const db = admin.firestore();
 
-module.exports = (robot) => {
-  robot.respond(/cron/, (_) => {
+module.exports = robot => {
+  robot.respond(/cron/, _ => {
     robot
       .http(
         `https://slack.com/api/channels.list?token=${process.env.HUBOT_SLACK_TOKEN}&exclude_archived=1`
       )
       .get()((err, res, body) => {
-        const channels = JSON.parse(body);
-        if (channels.ok) {
-          channels.channels
-          .filter(c => c.name === 'gjeservicebot-gje-workshop')
+      const channels = JSON.parse(body);
+      if (channels.ok) {
+        channels.channels
+          .filter(c => c.name === "gjeservicebot-gje-workshop")
           .map(c => c.members)[0]
           .forEach(id =>
             robot.messageRoom(
@@ -31,16 +28,16 @@ module.exports = (robot) => {
               'Reminder: Standup! :loudspeaker:\n\n \n\nFormat: _standup_ "hva du gjorde i går" "hva du skal gjøre i dag" "hvilke hindringer du har"'
             )
           );
-        }
-      });
+      }
+    });
   });
 
-  robot.respond(/standup (.*) (.*) (.*)/, (res) => {
+  robot.respond(/standup (.*) (.*) (.*)/, res => {
     const [, yesterday, today, obstacles] = res.match;
 
-    db.collection('standup')
+    db.collection("standup")
       .doc(res.envelope.user.id)
-      .collection('standups')
+      .collection("standups")
       .add({
         date: Date.now(),
         yesterday,

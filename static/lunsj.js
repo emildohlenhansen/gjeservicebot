9fde3b2568687313aa404bd922737f7a8373a420
Edvard1337 <edvardblunck@hotmail.com>
2019-12-03 09:32:13 +0100

    Update lunsj.js

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,17 +1,17 @@
 module.exports = {
  huset: {
-    mandag: "Lapskaus med flatbrød, Klippfiskgryte,  Tom Yum Gai suppe med tilbehør og brød",
-    tirsdag: "Shepherdspai med grov potetmos, Fiskeburger på grovbrød med råkost, Fiskesuppe med tilbehør og brød",
-    onsdag: "Brennsnut, Dampet torsk med stekte gulrøtter og bulgur, Løksuppe med tilbehør og brød",
-    torsdag: "Aloo Gobi (Indisk potet og blomkålgryte), Pasta med laks, Tilbehør: Raita, 'Potet og purreløksuppe' med tilbehør og brød",
-    fredag: "Entrecote med søtpotetchips, bearnaisesaus og bakte grønnsaker, Dagens fisk med tilbehør,  Dagens suppe med tilbehør og brød"
+    mandag: "Seibiff med løk, Grillet kyllinglår, Soppsuppe med tilbehør og brød",
+    tirsdag: "Pasta med kjøttsaus, Dampet torsk med sitronsaus, Krydret linsesuppe med tomater og kikerter, tilbehør og brød",
+    onsdag: "Grønnsakslasagne, Red curry fiskeform med nudler, Fiskesuppe med tilbehør og brød",
+    torsdag: "Libabrød med kjøttdeig, salat og dressing, Dagens fisk med tilbehør,  Meksikansk tomatsuppe med tilbehør og brød",
+    fredag: "Viltgryte med rosenkål, kokte småpoteter og tyttebær, Dagens suppe med tilbehør og brød"
   },
   galleriet: {
-		mandag: "Quorn Lasagne, Pølsesuppe",
-		tirsdag: "Svinebog med kål og bønner cassoulet, Minestronesuppe",
-		onsdag: "Fiskekaker med råkost og poteter, Paprika og greskar med chilli",
-		torsdag: "Amerikansk Thanksgiving Kalkun, Fiskesuppe med chili ,kokos og sitrongress",
-		fredag: "Pasta Alfredo, Thaisuppe"
+		mandag: "Pasta med sopp og linser Bolognese, Kalkun Tetrazzini Suppe",
+		tirsdag: "Brun lapskaus med flatbrød, Pastinaksuppe",
+		onsdag: "Laks med grønnsaker og risotto, Ertersuppe med svineknoke",
+		torsdag: "Husets Kyllingkjøttboller med erterstuing og poteter, Aspargesuppe",
+		fredag: "Pølser Ratatouille med bokhvete, Kylling tikka suppe"
   },
   rom: [
     'CA22ZT7TN'

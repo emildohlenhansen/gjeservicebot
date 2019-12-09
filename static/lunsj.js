c749f09bc5d65c990506baccc1ca96506138b5c2
Edvard1337 <edvardblunck@hotmail.com>
2019-12-09 09:36:09 +0100

    Update lunsj.js

--- static/lunsj.js
+++ static/lunsj.js
@@ -1,17 +1,17 @@
 module.exports = {
  huset: {
-    mandag: "Seibiff med løk, Grillet kyllinglår, Soppsuppe med tilbehør og brød",
-    tirsdag: "Pasta med kjøttsaus, Dampet torsk med sitronsaus, Krydret linsesuppe med tomater og kikerter, tilbehør og brød",
-    onsdag: "Grønnsakslasagne, Red curry fiskeform med nudler, Fiskesuppe med tilbehør og brød",
-    torsdag: "Libabrød med kjøttdeig, salat og dressing, Dagens fisk med tilbehør,  Meksikansk tomatsuppe med tilbehør og brød",
-    fredag: "Viltgryte med rosenkål, kokte småpoteter og tyttebær, Dagens suppe med tilbehør og brød"
+    mandag: "Kyllingwok, Torsk med topping, Aspargessuppe med tilbehør og brød",
+    tirsdag: "Jambalaya, Klippfiskgryte,  Meksikansk tomatsuppe med tilbehør og brød",
+    onsdag: "Hvalgryte med potetmos og grønnsaker, Pasta med fisk og friske grønnsaker, Potet og purreløksuppe med tilbehør og brød",
+    torsdag: "Indisk curry med mais, kikerter og pitabrød, salat og dressing. Dagens fisk med tilbehør,  Dagens suppe med tilbehør og brød",
+    fredag: "Juletallerken med tilbehør,  Fiskesuppe med tilbehør og brød"
   },
   galleriet: {
-		mandag: "Pasta med sopp og linser Bolognese, Kalkun Tetrazzini Suppe",
-		tirsdag: "Brun lapskaus med flatbrød, Pastinaksuppe",
-		onsdag: "Laks med grønnsaker og risotto, Ertersuppe med svineknoke",
-		torsdag: "Husets Kyllingkjøttboller med erterstuing og poteter, Aspargesuppe",
-		fredag: "Pølser Ratatouille med bokhvete, Kylling tikka suppe"
+		mandag: "Ungarsk lesco med potetpannekaker, Tomat og basillikum Suppe",
+		tirsdag: "Kylling Chop suey med ris, Pepperrotsuppe",
+		onsdag: "Tandoori Ørret, Kyllingsuppe",
+		torsdag: "Juletallerken, Kremetsoppsuppe" ,
+		fredag: "Kylling Quesadillas, Mais og chillisuppe"
   },
   rom: [
     'CA22ZT7TN'
